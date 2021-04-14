4ffe52a00cbd0f462d9e37e101e6a625682291c0
Raehyeon0208 <82011085+Raehyeon0208@users.noreply.github.com>
2021-04-14 16:03:06 +0900

    Add files via upload

--- RSPGame.java
+++ RSPGame.java
@@ -30,7 +30,7 @@ class RSPGame{
       		int nUser = scv.nextInt();
 	     	int nCom = MyRandom(1,3);   
       		CurrDisplay(nUser,nCom);
-	     	if(nUser == 1){ // 가위바위보 판단
+	     	if(nUser == 1){                                           // 가위바위보 판단
        		     if(nCom == 1) ScrPrint("비김 다시입력"); 
 	       	     if(nCom == 2) { ScrPrint("컴퓨터가 이김"); winner = 1;}
        		     if(nCom == 3) { ScrPrint("사용자가 이김"); winner = 0;}
@@ -54,7 +54,7 @@ class RSPGame{
 		ScrPrint("묵찌빠 시작~");
 		ScrPrint("1:가위 2:바위 3:보 중 하나를 입력하세요~");
 	while(true){
-		String Swinner = ""; // 이긴 사람 출력위한 변수
+		String Swinner = "";                                 // 이긴 사람 출력위한 변수
 		if(winner == 1)   
 			Swinner = "컴퓨터"; 
 		else{
@@ -64,27 +64,27 @@ class RSPGame{
 		int nUser2 = scv.nextInt();
 		int nCom2 = MyRandom(1,3);
 		CurrDisplay(nUser2,nCom2); 	
-		if(nUser2 == 1){                    // 묵찌빠 판단 비길 시 끝, 이긴 사람 공격권
-	             	if(nCom2 == 2) winner = 1;  // 컴퓨터 승리
-       		 	if(nCom2 == 3) winner = 0;  // 사용자 승리
+		if(nUser2 == 1){                                          // 묵찌빠 판단 비길 시 끝, 이긴 사람 공격권
+	             	if(nCom2 == 2) winner = 1;                         // 컴퓨터 승리
+       		 if(nCom2 == 3) winner = 0;          // 사용자 승리
 	              }
 		if(nUser2 == 2){
 	             	if(nCom2 == 1) winner = 0;  //사용자 승리
-       		 	if(nCom2 == 3) winner = 1;  //컴퓨터 승리
+       		 if(nCom2 == 3) winner = 1;  //컴퓨터 승리
 	              }
 		if(nUser2 == 3){
 	             	if(nCom2 == 1) winner = 1;  //컴퓨터 승리
-       		 	if(nCom2 == 2) winner = 0;  //사용자 승리
+       		 if(nCom2 == 2) winner = 0;  //사용자 승리
 	              }
 		if(nUser2 == nCom2){   //이긴 사람 출력
-			ScrPrint(Swinner + "가 승리!");
-			break; 
+		ScrPrint(Swinner + "가 승리!");
+		break; 
 		}
 		if(winner == 0){    // 공격권 출력
-			ScrPrint("사용자 공격권");
+		ScrPrint("사용자 공격권");
 		}
 		if(winner == 1){    // 공격권 출력
-			ScrPrint("컴퓨터 공격권");
+		ScrPrint("컴퓨터 공격권");
 		}
 	}
    }
